8fef7b1e720869e0bfa4b49f597d1cbf125136d8
Faycel Bilel <faycelbilel02@gmail.com>
2025-12-05 15:05:19 +0100

    fix routing operating hours

---
+++ src/app/(dashboard)/restaurants/operating-hours/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import OperatingHours from "@/app/(dashboard)/restaurants/RestaurantOperating/OperatingHours";
+
+export default function OperatingHoursRoute() {
+  return <OperatingHours />;
+}
